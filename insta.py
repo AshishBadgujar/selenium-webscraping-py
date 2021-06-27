a19f78c9e3275b1d0b4fcab663f829bfcc2aae3c
ashish <ashishbadgujar124@gmail.com>
2021-06-27 13:20:25 +0530

    new

--- insta.py
+++ insta.py
@@ -4,23 +4,23 @@ from bs4 import BeautifulSoup
 import urllib
 import requests
 
-username=input("enter instagram username:")
+username = input("enter instagram username:")
 
-options=Options()
-options.add_argument("--headless")
+options = Options()
+# options.add_argument("--headless")
 
-driver=webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(chrome_options=options)
 
 driver.get("https://www.instagram.com/"+username+"/")
 
-body=driver.find_element_by_tag_name('body')
-page=body.get_attribute('innerHTML')
+body = driver.find_element_by_tag_name('body')
+page = body.get_attribute('innerHTML')
 
-soup=BeautifulSoup(page,'html.parser')
-userimg=soup.select('img[alt="'+username+'\'s profile picture"]')
+soup = BeautifulSoup(page, 'html.parser')
+userimg = soup.select('img[alt="'+username+'\'s profile picture"]')
 
 if not userimg:
-    userimg=soup.select('.be6sR')
+    userimg = soup.select('.be6sR')
 
-urllib.request.urlretrieve(userimg[0]["src"],'userProfile.jpg')
+urllib.request.urlretrieve(userimg[0]["src"], 'userProfile.jpg')
 driver.close()
